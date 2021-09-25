0628bf9399534c4656fd7d60cc5947cc95270ac7
chuan <990265261@qq.com>
2021-09-25 19:13:17 +0800

    2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
-  
-
+  <div class="div">
+    <slot v-for="(vnode, key) in dtSlots" :name="key" :vm="123"></slot>
+  </div>
 </template>
 
 <script>
@@ -8,10 +9,62 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      rTime: 20,
+      sTime: 19,
+      cacheTime: 0
+    };
+  },
+  computed: {
+    dtSlots() {
+      let res = {};
+      Object.keys(this.$scopedSlots).forEach(e => {
+        if (e === "aaa") {
+          res[e] = this.$slots[e];
+        }
+      });
+      return res;
+    }
+  },
+  mounted() {
+    console.log(this);
+  },
+  methods: {
+    test() {
+      console.log(1);
+    },
+    openTime() {
+      const test = () => {
+        const aaa = time => {
+          if (time - this.cacheTime >= 1000) {
+            this.rTime -= 1;
+            this.cacheTime = time;
+          }
+          window.requestAnimationFrame(aaa);
+        };
+        window.requestAnimationFrame(aaa);
+      };
+      test();
+      setInterval(() => {
+        this.sTime -= 1;
+      }, 1000);
+    },
+    openLang() {
+      for (let i = 1; i < 100000; i++) {
+        console.log(i);
+      }
+    }
   }
 };
 </script>
 
 <style scoped lang="scss">
+@import url("//at.alicdn.com/t/font_2411645_2c7zja5bkxp.css");
+div {
+  display: flex;
 
+  color: red;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <HelloWorld>
+      <h1 slot="bbb">我是插槽aaa</h1>
+      <h1 slot="aaa" slot-scope="{vm}">我是插槽bbb {{vm}}</h1>
+    </HelloWorld>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import { a, b } from "@/test/test";
+console.log(a, b);
 export default {
   name: "Home",
   components: {
     HelloWorld
   },
-  methods: {
-    aaa() {}
-  }
+  methods: {}
 };
 </script>
