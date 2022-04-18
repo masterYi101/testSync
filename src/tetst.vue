5dff5b08d04c20de7cea5c0ef9553a76dc627ee7
lichuanchuan <lichuanchuan>
2022-04-18 13:13:01 +0800

    111

---
+++ src/tetst.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "tetst",
+  props: {},
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
